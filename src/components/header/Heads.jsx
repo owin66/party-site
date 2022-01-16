a3588ea8ac48d5dbb5212c3a11e4b7e2c2cd07e7
owin66 <78900270+owin66@users.noreply.github.com>
2022-01-16 17:12:20 +0200

    adding logics

---
+++ src/components/header/Heads.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Heads = () => {
+    return (
+        <div>
+            <span className="badge bg-primary">
+                <h3>12 человек тусанет с тобой сегодня</h3>
+            </span>
+        </div>
+    );
+};
+
+export default Heads;
