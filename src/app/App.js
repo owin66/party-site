2000cff893c1d0d7ba9fc46fa385d3b9c6de6217
owin66 <78900270+owin66@users.noreply.github.com>
2022-01-23 14:14:17 +0200

    refactoring

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Users from "./components/users";
-import SearchStatus from "./components/searchStatus";
 import api from "./api";
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
         setUsers(
             users.map((user) => {
                 if (user._id === id) {
-                    return { ...user, bookmark: !user.bookmark };
+                    return {...user, bookmark: !user.bookmark};
                 }
                 return user;
             })
@@ -21,7 +20,6 @@ function App() {
     };
     return (
         <div>
-            <SearchStatus length={users.length} />
             <Users
                 onDelete={handleDelete}
                 onToggleBookMark={handleToggleBookMark}
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -5,16 +5,20 @@ import Pagination from "./pagination";
 import User from "./user";
 import GroupList from "./groupList";
 import api from '../api'
+import SearchStatus from "./searchStatus";
 
 const Users = ({users: allUsers, ...rest}) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfessions] = useState()
     const [selectedProf, setSelectedProf] = useState()
-    const count = allUsers.length;
-    const pageSize = 4;
-    useEffect(() => {api.professions.fetchAll().then((data) =>
+    const pageSize = 2;
+    useEffect(() => {
+        api.professions.fetchAll().then((data) =>
             setProfessions(data))
     }, [])
+    useEffect(() => {
+        setCurrentPage(1)
+    }, [selectedProf])
 
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
@@ -27,6 +31,7 @@ const Users = ({users: allUsers, ...rest}) => {
     const filteredUsers = selectedProf
         ? allUsers.filter((user) => user.profession === selectedProf)
         : allUsers;
+    const count = filteredUsers.length;
 
     const usersCrop = paginate(filteredUsers, currentPage, pageSize);
     const clearFilter = () => {
@@ -34,45 +39,50 @@ const Users = ({users: allUsers, ...rest}) => {
     }
 
     return (
-        <>
+        <div className="d-flex">
             {professions && (
-                <div>
-                <GroupList selectedItem={selectedProf}
-                           items={professions}
-                           onItemSelect={handleProfessionSelect}
-                />
+                <div className="d-flex flex-column flex-shrink-o p-3">
+                    <GroupList selectedItem={selectedProf}
+                               items={professions}
+                               onItemSelect={handleProfessionSelect}
+                    />
                     <button className='btn btn-secondary mt-2'
-                            onClick={clearFilter}>Очистить</button>
+                            onClick={clearFilter}>Очистить
+                    </button>
                 </div>
             )}
-
-            {count > 0 && (
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Качества</th>
-                        <th scope="col">Провфессия</th>
-                        <th scope="col">Встретился, раз</th>
-                        <th scope="col">Оценка</th>
-                        <th scope="col">Избранное</th>
-                        <th/>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {usersCrop.map((user) => (
-                        <User {...rest} {...user} key={user._id}/>
-                    ))}
-                    </tbody>
-                </table>
-            )}
-            <Pagination
-                itemsCount={count}
-                pageSize={pageSize}
-                currentPage={currentPage}
-                onPageChange={handlePageChange}
-            />
-        </>
+            <div className="d-flex flex-column">
+                <SearchStatus length={count}/>
+                {count > 0 && (
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Провфессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th/>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {usersCrop.map((user) => (
+                            <User {...rest} {...user} key={user._id}/>
+                        ))}
+                        </tbody>
+                    </table>
+                )}
+                <div className="d-flex justify-content-center">
+                    <Pagination
+                        itemsCount={count}
+                        pageSize={pageSize}
+                        currentPage={currentPage}
+                        onPageChange={handlePageChange}
+                    />
+                </div>
+            </div>
+        </div>
     );
 };
 
